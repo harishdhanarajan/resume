ddcde179be8ac3c7571e2f4929ba71ddac02cc0e
Harish Dhanarajan <42219646+harishdhanarajan@users.noreply.github.com>
2024-04-17 23:27:16 +0530

    Update resume.py

--- resume.py
+++ resume.py
@@ -6,7 +6,21 @@ def local_css(file_name):
         st.markdown('<style>{}</style>'.format(f.read()), unsafe_allow_html=True)
 
 local_css("r_style/style.css")
+def gradient(color1, color2, color3, content1, content2):
+    st.markdown(f'<h1 style="text-align:center;background-image: linear-gradient(to right,{color1}, {color2});font-size:60px;border-radius:2%;">'
+                f'<span style="color:{color3};">{content1}</span><br>'
+                f'<span style="color:white;font-size:17px;">{content2}</span></h1>', 
+                unsafe_allow_html=True)
 
+with st.container():
+    col1,col2 = st.columns([8,3])
+
+full_name = info['Full_Name']
+with col1:
+    gradient('#FFD4DD','#000395','e0fbfc',f"Hi, I'm {full_name}👋", info["Intro"])
+    st.write("")
+    st.write(info['About'])
+    
 with st.container():
     st.subheader('⚒️ Skills')
     col1, col2, col3, col4, col5, col6, col7, col8 = st.columns([1,0.1, 1, 0.1, 1, 0.1, 1, 0.1])
