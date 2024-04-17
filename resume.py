ded05ec8b088f8f4bde34842bf8dffd6186ddb93
Harish Dhanarajan <42219646+harishdhanarajan@users.noreply.github.com>
2024-04-17 23:22:23 +0530

    Update resume.py

--- resume.py
+++ resume.py
@@ -9,7 +9,7 @@ local_css("r_style/style.css")
 
 with st.container():
     st.subheader('⚒️ Skills')
-    col1, col2, col3, col4, col5 = st.columns([1, 1, 1, 1, 1])
+    col1, col2, col3, col4 = st.columns([1, 1, 1, 1])
     with col1:
         st.image('images/python.png', width = 100)
     with col2:
@@ -18,13 +18,13 @@ with st.container():
         st.image('images/ml.PNG', width = 100)
     with col4:
         st.image('images/oracle.PNG', width = 100)
-    with col5:
-        st.image('images/snowflake.PNG', width = 100)
     with col1:
-        st.image('images/datalake.PNG', width = 100)
+        st.image('images/snowflake.PNG', width = 100)
     with col2:
-        st.image('images/powerbi.PNG', width = 100)
+        st.image('images/datalake.PNG', width = 100)
     with col3:
+        st.image('images/powerbi.PNG', width = 100)
+    with col4:
         st.image('images/github.png', use_column_width = 'auto')
     
     
