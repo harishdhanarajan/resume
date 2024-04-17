5d26a65fdb0e5d70ac5fd2efed155b4eb0d686b5
Harish Dhanarajan <42219646+harishdhanarajan@users.noreply.github.com>
2024-04-17 22:53:08 +0530

    Update resume.py

--- resume.py
+++ resume.py
@@ -1,13 +1,5 @@
 import requests
-from streamlit_lottie import st_lottie
 import streamlit as st
-import sys
-# Load a Lottie animation from a URL
-def load_lottieurl(url: str):
-    r = requests.get(url)
-    if r.status_code != 200:
-        return None
-    return r.json()
 
 def local_css(file_name):
     with open(file_name) as f:
