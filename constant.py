87c143d4939d3e14ec1658704bb513fac692fcf4
Harish Dhanarajan <42219646+harishdhanarajan@users.noreply.github.com>
2024-04-17 23:45:18 +0530

    Update constant.py

--- constant.py
+++ constant.py
@@ -3,7 +3,7 @@ info = {
    "Name": "Harish",
    "Full_Name": "Harish Dhanarajan",
    "Intro": "Data Science and AI/ML Enthusiast",
-   "About":"Hey there, I'm Vicky! I'm passionate about leveraging data and technology to drive meaningful insights and solutions in business. Check out my free courses and projects on cognitiveclass.ai: https://cognitiveclass.ai/",
+   "About":"Hello! I'm passionate about leveraging technology to solve complex problems and make processes more efficient. With a strong foundation in machine learning, Python programming, and expertise in OpenAI, I thrive on exploring the realms of artificial intelligence and automation. My journey in data science and analytics has equipped me with the skills to extract meaningful insights from data, empowering businesses to make informed decisions. Proficient in tools like PowerBI, I specialize in transforming raw data into actionable visualizations, unlocking hidden patterns and trends. Driven by curiosity and a commitment to innovation, I'm dedicated to staying at the forefront of emerging technologies and applying them to real-world challenges. Let's collaborate to turn ideas into impactful solutions!",
    "City":"Chennai, Tamilnadu",
    "Phone":"+91 7094086003",
    "Email": "harish.dhanarajan@gmail.com"
