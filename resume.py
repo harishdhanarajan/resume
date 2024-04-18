2cd31f7d7e8ec035bae36dfa3096e61e0293cd1b
Harish Dhanarajan <42219646+harishdhanarajan@users.noreply.github.com>
2024-04-19 00:25:05 +0530

    Update resume.py

--- resume.py
+++ resume.py
@@ -120,8 +120,9 @@ with st.container():
 
 st.markdown("---")
 st.subheader("Other Activities")
-st.write("dfd")
-st.write("df")
+st.write(" - Currently Developing a AI model for use of Solar Irradiance Prediction, Fault Diagnosis and Troubleshooting of Solar Power Systems")
+st.write(" - Taken Initivative to find parameters that impacts Nifty 50 by performing sentiment analysis using X with open/close points and cross comparing the performance in realtime")
+
 st.markdown("---")
 st.subheader("📨 Contact Me")
 st.write(info['Email'])
